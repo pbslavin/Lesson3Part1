da274f7adadd0038029cebffb16be86546c1c435
Peter Slavin <pbslavin@gmail.com>
2018-08-05 21:11:12 -0500

    added Lesson3Part1

--- Lesson3Part1/Program.cs
+++ Lesson3Part1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading.Tasks;
+using ThreadingAndTasks;
 
 namespace Lesson3Part1
 {
@@ -6,7 +8,42 @@ namespace Lesson3Part1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Fibonacci fibonacci2 = new Fibonacci(2);
+            Fibonacci fibonacci4 = new Fibonacci(4);
+            Fibonacci fibonacci8 = new Fibonacci(8);
+            Fibonacci fibonacci16 = new Fibonacci(16);
+
+            Task task1 = new Task(() =>
+            {
+                Console.WriteLine(($"The 2nd Fibonacci number is {fibonacci2.Calculate()}"));
+            });
+
+            Task task2 = new Task(() =>
+            {
+                Console.WriteLine(($"The 4th Fibonacci number is {fibonacci4.Calculate()}"));
+            });
+            Task task3 = new Task(() =>
+            {
+                Console.WriteLine(($"The 8th Fibonacci number is {fibonacci8.Calculate()}"));
+            });
+            Task task4 = new Task(() =>
+            {
+                Console.WriteLine($"The 16th Fibonacci number is {fibonacci16.Calculate()}");
+            });
+
+            task1.Start();
+            task1.Wait();
+            task2.Start();
+            task2.Wait();
+            task3.Start();
+            task3.Wait();
+            task4.Start();
+            task4.Wait();
+
+            foreach (int element in Fibonacci.cache)
+            {
+                Console.WriteLine(element);
+            }
         }
     }
 }
---
+++ ThreadingAndTasks/Fibonacci.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ThreadingAndTasks
+{
+    public class Fibonacci
+    {
+        private int _position;
+        public static List<int> cache = new List<int>();
+        public Fibonacci(int position)
+        {
+            _position = position;
+        }
+        public int Calculate()
+        {
+            int current = 1;
+            int last = 0;
+            int temp;
+
+            if (_position == 1)
+            {
+                cache.Add(current);
+                return 0;
+            }
+            else
+            {
+                for (int i = 0; i < _position - 2; i++)
+                {
+                    temp = current;
+                    current += last;
+                    last = temp;
+                }
+            }
+            cache.Add(current);
+            return current;
+        }
+    }
+}
